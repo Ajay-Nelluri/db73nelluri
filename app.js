790acd4ec59c89d578968f1a0391f737973ae4c6
Ajay-Nelluri <S543773@nwmissouri.edu>
2021-12-01 19:42:01 -0600

    changes made to app.js

--- app.js
+++ app.js
@@ -3,55 +3,31 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+const mongoose = require("mongoose");
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var planeRouter = require('./routes/plane');
-var addmodsRouter = require('./routes/addmods')
-var selectorRouter = require('./routes/selector');
-var resourceRouter = require('./routes/resource');
-var boilingstarRouter = require('./routes/boilingstar');
-
-var Costume = require('./models/costume');
-var app = express();
+const carRouter = require('./routes/car')
+const resoureRouter = require('./routes/resource')
+const addModRouter = require('./routes/addmods');
+const selectorRouter = require('./routes/selector');
+const Costume = require("./models/costume");
+const dcComicsRouter = require("./routes/dccomics");
+var Account = require('./models/account'); 
 
 var passport = require('passport'); 
 var LocalStrategy = require('passport-local').Strategy; 
+var app = express();
 
-passport.use(new LocalStrategy( 
-  function(username, password, done) { 
-    Account.findOne({ username: username }, function (err, user) { 
-      if (err) { return done(err); } 
-      if (!user) { 
-        return done(null, false, { message: 'Incorrect username.' }); 
-      } 
-      if (!user.validPassword(password)) { 
-        return done(null, false, { message: 'Incorrect password.' }); 
-      } 
-      return done(null, user); 
-    }); 
-  } 
-))
-  
- 
+const connectionString =  process.env.MONGO_CON;
+console.log(connectionString)
+mongoose.connect(connectionString,{useNewUrlParser: true, useUnifiedTopology: true}); 
 
-const connectionString =
-  process.env.MONGO_CON
-mongoose = require('mongoose');
-mongoose.connect(connectionString,
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  });
-
-//Get the default connection
-var db = mongoose.connection;
-//Bind connection to error event
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once("open", function () {
-  console.log("Connection to DB succeeded")
-});
-recreateDB();
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:')); 
+db.once("open", function(){
+   console.log("Connection to DB succeeded");
+    recreateDB();
+}); 
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -61,7 +37,6 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-
 app.use(require('express-session')({ 
   secret: 'keyboard cat', 
   resave: false, 
@@ -69,35 +44,22 @@ app.use(require('express-session')({
 })); 
 app.use(passport.initialize()); 
 app.use(passport.session()); 
- 
-
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/resource', resoureRouter);
+app.use('/dc-comics', dcComicsRouter);
 app.use('/users', usersRouter);
-app.use('/plane', planeRouter);
-app.use('/addmods', addmodsRouter);
+app.use('/car', carRouter);
+app.use('/addmods', addModRouter);
 app.use('/selector', selectorRouter);
-app.use('/resource', resourceRouter);
-app.use('/boilingstar', boilingstarRouter);
-
-// passport config 
-// Use the existing connection 
-// The Account model  
-var Account =require('./models/account'); 
- 
-passport.use(new LocalStrategy(Account.authenticate())); 
-passport.serializeUser(Account.serializeUser()); 
-passport.deserializeUser(Account.deserializeUser()); 
- 
-
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function (err, req, res, next) {
+app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -107,25 +69,55 @@ app.use(function (err, req, res, next) {
   res.render('error');
 });
 
+passport.use(new LocalStrategy( 
+  function(username, password, done) { 
+    Account.findOne({ username: username }, function (err, user) { 
+      if (err) { return done(err); } 
+      if (!user) { 
+        return done(null, false, { message: 'Incorrect username.' }); 
+      } 
+      console.log(user)
+      if ( user.validPassword && !user.validPassword(password)) { 
+        return done(null, false, { message: 'Incorrect password.' }); 
+      } 
+      return done(null, user); 
+    }); 
+  }))
+
+  passport.serializeUser(function(user, done) {
+    done(null, user.id);
+  });
+  
+  passport.deserializeUser(function(id, done) {
+    Account.findById(id, function(err, user) {
+      done(err, user);
+    });
+  });
+
+
+async function recreateDB(){ 
+  // Delete everything 
+  await Costume.deleteMany(); 
+ 
+  let instance1 = new Costume({costume_type:"ghost",  size:'large', cost:125.4}); 
+  let instance2 = new Costume({costume_type:"Joker",  size:'Medium', cost:129.4}); 
+  let instance3 = new Costume({costume_type:"Batman",  size:'small', cost:112.4}); 
+
+  instance1.save( function(err,doc) { 
+      if(err) return console.error(err); 
+      console.log("First object saved") 
+  });
+  instance2.save( function(err,doc) { 
+    if(err) return console.error(err); 
+    console.log("second object saved") 
+  });
+  instance3.save( function(err,doc) { 
+    if(err) return console.error(err); 
+    console.log("third object saved") 
+  });
+  
+} 
+ 
+
+
 module.exports = app;
-
-async function recreateDB() {
-  // Delete everything
-  await Costume.deleteMany();
-  let instance1 = new Costume({ costume_type: "hulk", size: 'large', cost: 25.4 });
-  let instance2 = new Costume({ costume_type: "superman", size: 'medium', cost: 35.4 });
-  let instance3 = new Costume({ costume_type: "ironman", size: 'small', cost: 30.4 });
-
-  instance1.save(function (err, doc) {
-    if (err) return console.error(err);
-    console.log("First object saved")
-  });
-  instance2.save(function (err, doc) {
-    if (err) return console.error(err);
-    console.log("Second object saved")
-  });
-  instance3.save(function (err, doc) {
-    if (err) return console.error(err);
-    console.log("Third object saved")
-  });
-}
